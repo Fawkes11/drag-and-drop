4fec40d31cb09514b52e8fa81e35dd50826f9f8f
Hessler Inampuez <hesslerinampuez@gmail.com>
2022-11-27 01:09:14 -0500

    creation of the onDragEnd function

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,44 @@ const App = () => {
     setData(initialData);
   }, []);
 
-  const onDragEnd = () => {
-    //TODO: reoder our column
-  }
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    //si lo mueve a un destino no permitido
+    if (!destination) {
+      return;
+    }
+    //para mover entre contenedores y diferente posicion.
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = data.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    setData((prevData) => {
+      return {
+        ...prevData,
+        columns: {
+          ...prevData.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+    }); 
+  };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}
-    >
+    <DragDropContext onDragEnd={onDragEnd}>
       {data.columnOrder?.map((columnId) => {
         const column = data.columns[columnId];
         const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
