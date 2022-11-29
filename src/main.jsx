461952e59c836c2ed6df534a375508b2d5ddab88
Hessler Inampuez <hesslerinampuez@gmail.com>
2022-11-29 16:18:39 -0500

    Reset CSS added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import CssBaseline from '@mui/material/CssBaseline';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    <>
+        <CssBaseline />
+        <App />
+    </>
 
 )
